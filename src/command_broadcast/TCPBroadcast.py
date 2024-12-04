8005468fab0d6fb5f1c7fa60f7df8d105d2168ef
scheysej <scheysej@udmercy.edu>
2024-12-04 15:06:32 -0500

    some changes

--- src/command_broadcast/TCPBroadcast.py
+++ src/command_broadcast/TCPBroadcast.py
@@ -10,9 +10,10 @@ def broadcast_message(message, hostIP, numOfRobots):
     # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.listen(5) #change to number of robots
 
+    print(f"Binding the ip to host: {hostIP}")
     sock.bind((hostIP, port))
+    sock.listen(5) #change to number of robots
 
     try:
         client_socket, address = sock.accept()
