0a0d810ed2600e00923d3bebf9d9583641b2f491
scheysej <scheysej@udmercy.edu>
2024-12-04 15:03:07 -0500

    changed comment to python syntax isntead of c++

--- src/command_broadcast/TCPBroadcast.py
+++ src/command_broadcast/TCPBroadcast.py
@@ -10,7 +10,7 @@ def broadcast_message(message, hostIP, numOfRobots):
     # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.listen(5) //change to number of robots
+    sock.listen(5) #change to number of robots
 
     sock.bind((hostIP, port))
 
